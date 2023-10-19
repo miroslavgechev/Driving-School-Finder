2ebe6f383a8433d987dd8b880f00204f44669c1d
Miroslav Gechev <vindaloo89@gmail.com>
2023-10-19 14:57:29 +0300

    Initial code and dependency updates

--- driving-school-finder/src/index.js
+++ driving-school-finder/src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Importing Roboto font
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { BrowserRouter } from 'react-router-dom';
 
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,4 +25,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
