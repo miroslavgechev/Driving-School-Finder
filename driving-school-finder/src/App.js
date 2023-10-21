a1d063e65b5ab58b6ffebd13f504cc9e89d85995
Miroslav Gechev <vindaloo89@gmail.com>
2023-10-21 21:54:06 +0300

    MUI Tests

--- driving-school-finder/src/App.js
+++ driving-school-finder/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 
 import * as React from 'react';
+
 import Button from '@mui/material/Button';
+import WifiIcon from '@mui/icons-material/Wifi';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Driving School Finder</h1>
       <Button variant="outlined" onClick={(e) => console.log(e.target)}>Hello world</Button>
+      <WifiIcon color="success"/>
+
     </div>
   );
 }
