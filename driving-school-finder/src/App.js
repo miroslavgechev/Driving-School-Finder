22bf568ce413e3429fdb546b9c23a55e4937954b
Miroslav Gechev <vindaloo89@gmail.com>
2023-10-25 15:07:13 +0300

    Setup clean installation

--- driving-school-finder/src/App.js
+++ driving-school-finder/src/App.js
@@ -4,14 +4,13 @@ import * as React from 'react';
 
 import Button from '@mui/material/Button';
 import WifiIcon from '@mui/icons-material/Wifi';
-import { updateDrivingSchool, addDrivingSchool } from './firebase';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Driving School Finder</h1>
-      <Button variant="outlined" onClick={() => addDrivingSchool()}>Hello world</Button>
+      <Button variant="outlined">Hello world</Button>
       <WifiIcon color="success" />
 
     </div>
