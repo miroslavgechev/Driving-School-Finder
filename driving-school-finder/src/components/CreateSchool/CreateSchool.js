a79201c3314255fa5aeece4d06ddb334e24c1131
Miroslav Gechev <vindaloo89@gmail.com>
2023-11-07 19:41:51 +0200

    Create School Component UI 90% Completed

--- driving-school-finder/src/components/CreateSchool/CreateSchool.js
+++ driving-school-finder/src/components/CreateSchool/CreateSchool.js
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 
 import Container from 'components/Container';
+import Headline from './components/Headline/Headline';
 import SchoolForm from './components/SchoolForm/SchoolForm';
 import ContactsForm from './components/ContactsForm/ContactsForm';
 import ImgForm from './components/ImgForm/ImgForm';
@@ -15,76 +16,83 @@ import CoursesForm from './components/CoursesForm/CoursesForm';
 
 const CreateSchool = () => {
   return (
-    <Container>
-      <Box>
-
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h6" gutterBottom fontWeight={700}>
-              Данни за автошколата
-            </Typography>
-            <Typography variant={'subtitle2'} color={'text.secondary'}>
-              Опиши своята автошкола, така че курсистите да разберат защо е най-подходяща за тях
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12} md={8}>
-            <SchoolForm />
-          </Grid>
-
-        </Grid>
-
-        <Divider sx={{ marginY: 4 }} />
-
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h6" gutterBottom fontWeight={700}>
-              Данни за контакт
-            </Typography>
-            <Typography variant={'subtitle2'} color={'text.secondary'}>
-              Въведи контактните данни на твоята автошкола
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12} md={8}>
-            <ContactsForm />
-          </Grid>
-        </Grid>
-
-        <Divider sx={{ marginY: 4 }} />
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h6" gutterBottom fontWeight={700}>
-              Снимки
-            </Typography>
-            <Typography variant={'subtitle2'} color={'text.secondary'}>
-              Качи снимки с високо качество, показвайки възможностите на школата
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12} md={8}>
-            <ImgForm />
-          </Grid>
-
-        </Grid>
-        <Divider sx={{ marginY: 4 }} />
-
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h6" gutterBottom fontWeight={700}>
-              Курсове
-            </Typography>
-            <Typography variant={'subtitle2'} color={'text.secondary'}>
-              Опиши кои курсове предлагате и техните цени
-            </Typography>
-          </Grid>
-
-          <Grid item xs={12} md={8}>
-            <CoursesForm />
-          </Grid>
-        </Grid>
+    <>
+      <Box bgcolor={'alternate.main'} >
+        <Container paddingY={{ xs: 2, sm: 2.5 }}>
+          <Headline />
+        </Container>
       </Box>
-    </Container>
+      <Container>
+
+        <Box>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={4}>
+              <Typography variant="h6" gutterBottom fontWeight={700}>
+                Данни за автошколата
+              </Typography>
+              <Typography variant={'subtitle2'} color={'text.secondary'}>
+                Опиши своята автошкола, така че курсистите да разберат защо е най-подходяща за тях
+              </Typography>
+            </Grid>
+
+            <Grid item xs={12} md={8}>
+              <SchoolForm />
+            </Grid>
+
+          </Grid>
+
+          <Divider sx={{ marginY: 4 }} />
+
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={4}>
+              <Typography variant="h6" gutterBottom fontWeight={700}>
+                Данни за контакт
+              </Typography>
+              <Typography variant={'subtitle2'} color={'text.secondary'}>
+                Въведи контактните данни на твоята автошкола
+              </Typography>
+            </Grid>
+
+            <Grid item xs={12} md={8}>
+              <ContactsForm />
+            </Grid>
+          </Grid>
+
+          <Divider sx={{ marginY: 4 }} />
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={4}>
+              <Typography variant="h6" gutterBottom fontWeight={700}>
+                Снимки
+              </Typography>
+              <Typography variant={'subtitle2'} color={'text.secondary'}>
+                Качи снимки с високо качество, показвайки възможностите на школата
+              </Typography>
+            </Grid>
+
+            <Grid item xs={12} md={8}>
+              <ImgForm />
+            </Grid>
+
+          </Grid>
+          <Divider sx={{ marginY: 4 }} />
+
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={4}>
+              <Typography variant="h6" gutterBottom fontWeight={700}>
+                Курсове
+              </Typography>
+              <Typography variant={'subtitle2'} color={'text.secondary'}>
+                Опиши курсовете с тяхната продължителност и цени
+              </Typography>
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <CoursesForm />
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </>
   );
 };
 
--- driving-school-finder/src/components/CreateSchool/components/CoursesForm/CoursesForm.js
+++ driving-school-finder/src/components/CreateSchool/components/CoursesForm/CoursesForm.js
@@ -8,8 +8,9 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-
-import Container from 'components/Container';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
 
 import { TABLE_SUBTITLES } from 'CONSTANTS';
 
@@ -20,70 +21,95 @@ const courses = ['A', 'B'];
 const CoursesForm = () => {
 
   return (
-    <Container paddingY={{ xs: 2, sm: 2.5 }}>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 750 }} aria-label="simple table">
-          <TableHead sx={{ bgcolor: 'alternate.dark' }}>
-            <TableRow>
-              <TableCell />
-              {courses.map((item, i) => (
-                <TableCell key={i} sx={{ textAlign: 'center' }}>
-                  <Typography
-                    variant={'subtitle2'}
-                    fontWeight={700}
-                  >
-                    Категория "{item}"
-                  </Typography>
-                </TableCell>
-              ))}
-              <TableCell />
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {Object.entries(subtitles).map(([key, value]) => (
-              <TableRow
-                key={key}
-                sx={{
-                  '&:last-child td, &:last-child th': { border: 0, bgcolor: 'text.alternate.dark' },
-                  '&:nth-of-type(2n)': { bgcolor: 'alternate.main' },
-                }}
-              >
-                <TableCell sx={{ width: '20%' }} component="th" scope="row">
-                  <Typography variant={'subtitle2'} fontWeight={700}>
-                    {value}
-                  </Typography>
-                </TableCell>
-
-                {courses.map((item, i) => (
-                  <TableCell key={i + item} sx={{ textAlign: 'center' }}>
-
-                    <Box display={'flex'} justifyContent={'center'} alignItems={'center'} gap={'1em'}>
-                      <TextField
-                        variant="outlined"
-                        name={item + '_' + key}
-                        sx={{ width: '20%', textAlign: 'center' }}
-                      />
-                      {' '}
-                      <Typography color={'text.secondary.contrastText'} variant={'subtitle2'} fontWeight={key === 'coursePrice' ? 900 : undefined} >
-                        {item[key]} {item[key] == 'Безплатен' ? '' : key.startsWith('hours') ? 'часа' : 'лв.'}
+    <Box>
+      <form>
+        <Grid container spacing={4} paddingY={{ xs: 2, sm: 2.5 }}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 750 }} aria-label="simple table">
+              <TableHead sx={{ bgcolor: 'alternate.dark' }}>
+                <TableRow>
+                  <TableCell />
+                  {courses.map((item, i) => (
+                    <TableCell key={i} sx={{ textAlign: 'center' }}>
+                      <Typography
+                        variant={'subtitle2'}
+                        fontWeight={700}
+                      >
+                        Категория "{item}"
                       </Typography>
-                    </Box>
+                    </TableCell>
+                  ))}
+                  <TableCell />
+                </TableRow>
+              </TableHead>
 
-                  </TableCell>
+              <TableBody>
+                {Object.entries(subtitles).map(([key, value]) => (
+                  <TableRow
+                    key={key}
+                    sx={{
+                      '&:last-child td, &:last-child th': { border: 0, bgcolor: 'text.alternate.dark' },
+                      '&:nth-of-type(2n)': { bgcolor: 'alternate.main' },
+                    }}
+                  >
+                    <TableCell sx={{ width: '20%' }} component="th" scope="row">
+                      <Typography variant={'subtitle2'} fontWeight={700}>
+                        {value}
+                      </Typography>
+                    </TableCell>
+
+                    {courses.map((item, i) => (
+                      <TableCell key={i + item} sx={{ textAlign: 'center' }}>
+
+                        <Box display={'flex'} justifyContent={'center'} alignItems={'center'} gap={'1em'}>
+                          <TextField
+                            variant="outlined"
+                            name={item + '_' + key}
+                            sx={{ width: '50%' }}
+                            inputProps={{ style: { textAlign: 'center' } }}
+                          />
+                          <Typography color={'text.secondary.contrastText'} variant={'subtitle2'} fontWeight={key === 'coursePrice' ? 900 : undefined} >
+                            {item[key]} {item[key] == 'Безплатен' ? '' : key.startsWith('hours') ? 'часа' : 'лева'}
+                          </Typography>
+                        </Box>
+
+                      </TableCell>
+                    ))}
+
+                    <TableCell>
+
+                    </TableCell>
+                  </TableRow>
                 ))}
+              </TableBody>
 
-                <TableCell>
+            </Table>
+          </TableContainer>
+          <Grid item container xs={12}>
+            <Box
+              display="flex"
+              flexDirection={{ xs: 'column', sm: 'row' }}
+              alignItems={{ xs: 'stretched', sm: 'center' }}
+              justifyContent={'space-between'}
+              width={1}
+              margin={'0 auto'}
+            >
+              <Box marginBottom={{ xs: 1, sm: 0 }}>
 
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-
-        </Table>
-      </TableContainer>
-    </Container>
-  );
+              </Box>
+              <Button
+                size={'large'}
+                variant={'contained'}
+                type={'submit'}
+                startIcon={<CloudUploadOutlinedIcon />}
+              >
+                Запази промените
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </form>
+    </Box >);
 };
 
 export default CoursesForm;
