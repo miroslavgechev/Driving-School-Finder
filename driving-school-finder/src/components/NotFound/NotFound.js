1969f12d106d40928ea1e56a9d8f1e9fc133cc33
Miroslav Gechev <vindaloo89@gmail.com>
2023-11-27 15:28:34 +0200

    Add logic to NotFound Component and add a css module file

--- driving-school-finder/src/components/NotFound/NotFound.js
+++ driving-school-finder/src/components/NotFound/NotFound.js
@@ -1,68 +1,47 @@
-import React from 'react';
-import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import useMediaQuery from '@mui/material/useMediaQuery';
 
 import Container from 'components/Container';
+import RightSidePhotoCover from 'components/shared/RightSidePhotoCover/RightSidePhotoCover';
+
+import { Link } from 'react-router-dom';
+import styles from './notFound.module.css';
 
 const NotFound = () => {
-  const theme = useTheme();
-  const isMd = useMediaQuery(theme.breakpoints.up('md'), {
-    defaultMatches: true,
-  });
 
   return (
-    <Box
-      sx={{
-        width: 1,
-        height: 1,
-        overflow: 'hidden',
-      }}
-    >
+    <Box className={styles.boxContainer}>
       <Container paddingX={0} paddingY={0} maxWidth={{ sm: 1, md: 1236 }}>
-        <Box
-          display={'flex'}
-          flexDirection={{ xs: 'column', md: 'row' }}
-          position={'relative'}
-        >
-          <Box
-            width={1}
-            order={{ xs: 2, md: 1 }}
-            display={'flex'}
-            alignItems={'center'}
-          >
+        <Box className={styles.outerBox}>
+          <Box className={styles.textBoxContainer}>
             <Container>
               <Box>
                 <Typography
-                  variant="h1"
+                  variant='h1'
                   component={'h1'}
-                  align={isMd ? 'left' : 'center'}
-                  sx={{ fontWeight: 700 }}
+                  className={`${styles.textBox} ${styles.headerText}`}
                 >
                   Опааа!
                 </Typography>
                 <Typography
-                  variant="h6"
-                  component="p"
-                  color="text.secondary"
-                  align={isMd ? 'left' : 'center'}
+                  variant='h6'
+                  component='p'
+                  color='text.secondary'
+                  className={styles.textBox}
                 >
                   Изглежда, че тази страница не съществува.
                 </Typography>
                 <Box
                   marginTop={4}
-                  display={'flex'}
-                  justifyContent={{ xs: 'center', md: 'flex-start' }}
+                  className={styles.buttonBoxContainer}
                 >
                   <Button
                     component={Link}
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    href={'/'}
+                    variant='contained'
+                    color='primary'
+                    size='large'
+                    to='/'
                   >
                     Начална страница
                   </Button>
@@ -71,70 +50,7 @@ const NotFound = () => {
             </Container>
           </Box>
 
-          <Box
-            sx={{
-              flex: { xs: '0 0 100%', md: '0 0 50%' },
-              position: 'relative',
-              maxWidth: { xs: '100%', md: '50%' },
-              order: { xs: 1, md: 2 },
-              minHeight: { xs: 'auto', md: 'calc(100vh - 58px)' },
-            }}
-          >
-            <Box
-              sx={{
-                width: { xs: 1, md: '50vw' },
-                height: '100%',
-                position: 'relative',
-              }}
-            >
-              <Box
-                sx={{
-                  width: '100%',
-                  height: '100%',
-                  overflow: 'hidden',
-                }}
-              >
-                <Box
-                  sx={{
-                    overflow: 'hidden',
-                    left: '0%',
-                    width: 1,
-                    height: 1,
-                    position: { xs: 'relative', md: 'absolute' },
-                    clipPath: {
-                      xs: 'none',
-                      md: 'polygon(10% 0%, 100% 0, 100% 100%, 0% 100%)',
-                    },
-                    shapeOutside: {
-                      xs: 'none',
-                      md: 'polygon(10% 0%, 100% 0, 100% 100%, 0% 100%)',
-                    },
-                  }}
-                >
-                  <Box
-                    sx={{
-                      height: { xs: 'auto', md: 1 },
-                      '& img': {
-                        objectFit: 'cover',
-                      },
-                    }}
-                  >
-                    <Box
-                      component={'img'}
-                      loading="lazy"
-                      src={
-                        'notfound.avif'
-                      }
-                      height={{ xs: 'auto', md: 1 }}
-                      maxHeight={{ xs: 300, md: 1 }}
-                      width={1}
-                      maxWidth={1}
-                    />
-                  </Box>
-                </Box>
-              </Box>
-            </Box>
-          </Box>
+          <RightSidePhotoCover imgSrc={'notfound.avif'} />
         </Box>
       </Container>
     </Box>
