15baa263ca1ec734c086641e5eda9694a8350144
Miroslav Gechev <vindaloo89@gmail.com>
2023-11-29 14:25:39 +0200

    Move styling to css, add shared Headline, add skeleton

--- driving-school-finder/src/components/ManageAccount/ManagerAccount.js
+++ driving-school-finder/src/components/ManageAccount/ManagerAccount.js
@@ -3,17 +3,17 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
+import Skeleton from '@mui/material/Skeleton';
 
 import Container from 'components/Container';
-import Headline from './components/Headline/Headline';
+import Headline from '../shared/Headline/Headline';
 import EditPersonalDataForm from './components/EditPersonalDataForm/EditPersonalDataForm';
 import ChangePasswordForm from './components/ChangePasswordForm/ChangePasswordForm';
 import ManageReviews from './components/ManageReviews/ManageReviews';
 import ManageSchools from './components/ManageSchools/ManageSchools';
 
 import { useAuthContext } from 'contexts/authContext';
-
-//TODO Add validation
+import styles from './manageAccount.module.css';
 
 const ManageAccount = () => {
 
@@ -21,26 +21,32 @@ const ManageAccount = () => {
 
   return (
     <>
-      <Box bgcolor={'alternate.main'} >
+      <Box bgcolor='alternate.main' >
         <Container paddingY={{ xs: 2, sm: 2.5 }}>
-          <Headline />
+          <Headline text={'Управление на профила'} />
         </Container>
       </Box>
       <Container>
-
         <Box>
           <Grid container spacing={4}>
             <Grid item xs={12} md={4}>
-              <Typography variant="h6" gutterBottom fontWeight={700}>
+              <Typography variant='h6' gutterBottom className={styles.headerText}>
                 Данни за профила
               </Typography>
-              <Typography variant={'subtitle2'} color={'text.secondary'}>
+              <Typography variant='subtitle2' color='text.secondary'>
                 Можеш да промениш личните данни на профила си
               </Typography>
             </Grid>
 
             <Grid item xs={12} md={8}>
-              <EditPersonalDataForm />
+              {user
+                ?
+                <EditPersonalDataForm />
+                :
+                <Box className={styles.skeletonEditPersonalDataForm}>
+                  <Skeleton variant="rectangular" className={styles.skeletonHeight} animation="wave" />
+                </Box>
+              }
             </Grid>
 
           </Grid>
@@ -49,10 +55,10 @@ const ManageAccount = () => {
 
           <Grid container spacing={4}>
             <Grid item xs={12} md={4}>
-              <Typography variant="h6" gutterBottom fontWeight={700}>
+              <Typography variant='h6' gutterBottom className={styles.headerText}>
                 Парола
               </Typography>
-              <Typography variant={'subtitle2'} color={'text.secondary'}>
+              <Typography variant='subtitle2' color='text.secondary'>
                 Можеш да промениш паролата си
               </Typography>
             </Grid>
@@ -68,10 +74,10 @@ const ManageAccount = () => {
             <>
               <Grid container spacing={4}>
                 <Grid item xs={12} md={4}>
-                  <Typography variant="h6" gutterBottom fontWeight={700}>
+                  <Typography variant='h6' gutterBottom className={styles.headerText}>
                     Твоите отзиви
                   </Typography>
-                  <Typography variant={'subtitle2'} color={'text.secondary'}>
+                  <Typography variant='subtitle2' color='text.secondary'>
                     Отзивите, които си оставил
                   </Typography>
                 </Grid>
@@ -88,10 +94,10 @@ const ManageAccount = () => {
           {user?.role === 'school' &&
             <Grid container spacing={4}>
               <Grid item xs={12} md={4}>
-                <Typography variant="h6" gutterBottom fontWeight={700}>
+                <Typography variant='h6' gutterBottom className={styles.headerText}>
                   Твоите автошколи
                 </Typography>
-                <Typography variant={'subtitle2'} color={'text.secondary'}>
+                <Typography variant='subtitle2' color='text.secondary'>
                   Управявай автошколите, които си създал
                 </Typography>
               </Grid>
@@ -101,7 +107,7 @@ const ManageAccount = () => {
               </Grid>
             </Grid>
           }
-          
+
         </Box>
       </Container>
     </>
