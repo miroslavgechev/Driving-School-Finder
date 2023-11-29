40bab0e1072a54f5f14c22266fb039bcddbff61d
Miroslav Gechev <vindaloo89@gmail.com>
2023-11-29 16:10:01 +0200

    Add logic to ChangePassword form component

--- driving-school-finder/src/components/ManageAccount/components/ChangePasswordForm/ChangePasswordForm.js
+++ driving-school-finder/src/components/ManageAccount/components/ChangePasswordForm/ChangePasswordForm.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
+import { useState } from 'react';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import Box from '@mui/material/Box';
@@ -7,13 +7,18 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
+import CircularProgress from '@mui/material/CircularProgress';
+import Alert from '@mui/material/Alert';
+
+import styles from './changePasswordForm.module.css';
+import { SUCCESS_STATES, ERROR_MESSAGES, ERROR_CODES } from 'CONSTANTS';
+import { useAuthContext } from 'contexts/authContext';
 
 const validationSchema = yup.object({
-  currentPassword: yup
+  oldPassword: yup
     .string()
     .required('Паролата е задължителна, за да знаем, че си ти')
-    .min(8, 'Паролата следва да е поне 8 символа')
-    .oneOf(['12345678'], 'Текущата парола е грешна'),
+    .min(8, 'Паролата следва да е поне 8 символа'),
   password: yup
     .string()
     .required('Паролата е задължителна, за да знаем, че си ти')
@@ -26,26 +31,46 @@ const validationSchema = yup.object({
 });
 
 const ChangePasswordForm = () => {
-  const roles = {
-    student: 1,
-    school: 2,
-  };
+
+  const { updateCredentials } = useAuthContext();
 
   const initialValues = {
-    currentPassword: '',
+    oldPassword: '',
     password: '',
     rePassword: '',
-    role: roles.student,
   };
 
-  const onSubmit = (values) => {
-    return values;
+  const [isLoading, setIsLoading] = useState(false);
+  const [successState, setSuccessState] = useState(SUCCESS_STATES.none);
+
+  const handleSubmit = async (values) => {
+    try {
+      formik.setStatus(null);
+      setIsLoading(true);
+      await updateCredentials(values);
+      setIsLoading(false);
+      setSuccessState(SUCCESS_STATES.success);
+    } catch (error) {
+      if (error.message.includes(ERROR_CODES.invalidCredential)) {
+        formik.setStatus(ERROR_MESSAGES.invalidCredential);
+      } else {
+        formik.setStatus(ERROR_MESSAGES.defaultError);
+      }
+
+      setIsLoading(false);
+      setSuccessState(SUCCESS_STATES.error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSuccessState(SUCCESS_STATES.error);
+    formik.handleChange(event);
   };
 
   const formik = useFormik({
     initialValues,
-    validationSchema: validationSchema,
-    onSubmit,
+    validationSchema,
+    onSubmit: handleSubmit,
   });
 
   return (
@@ -53,73 +78,84 @@ const ChangePasswordForm = () => {
       <form onSubmit={formik.handleSubmit}>
         <Grid container spacing={4}>
           <Grid item xs={12}>
-            <Typography variant={'subtitle2'} sx={{ marginBottom: 2 }}>
+            <Typography variant='subtitle2' sx={{ marginBottom: 2 }}>
               Въведи текущата парола
             </Typography>
             <TextField
-              label="Текуща парола *"
-              variant="outlined"
-              name={'currentPassword'}
-              type={'password'}
+              label='Текуща парола *'
+              variant='outlined'
+              name={'oldPassword'}
+              type='password'
               fullWidth
-              value={formik.values.currentPassword}
-              onChange={formik.handleChange}
-              error={formik.touched.currentPassword && Boolean(formik.errors.currentPassword)}
-              helperText={formik.touched.currentPassword && formik.errors.currentPassword}
+              value={formik.values.oldPassword}
+              onChange={handleChange}
+              error={formik.touched.oldPassword && Boolean(formik.errors.oldPassword)}
+              helperText={formik.touched.oldPassword && formik.errors.oldPassword}
             />
           </Grid>
 
           <Grid item xs={12} sm={6}>
-            <Typography variant={'subtitle2'} sx={{ marginBottom: 2 }}>
+            <Typography variant='subtitle2' sx={{ marginBottom: 2 }}>
               Въведи нова парола
             </Typography>
             <TextField
-              label="Нова парола *"
-              variant="outlined"
+              label='Нова парола *'
+              variant='outlined'
               name={'password'}
-              type={'password'}
+              type='password'
               fullWidth
               value={formik.values.password}
-              onChange={formik.handleChange}
+              onChange={handleChange}
               error={formik.touched.password && Boolean(formik.errors.password)}
               helperText={formik.touched.password && formik.errors.password}
             />
           </Grid>
 
           <Grid item xs={12} sm={6}>
-            <Typography variant={'subtitle2'} sx={{ marginBottom: 2 }}>
+            <Typography variant='subtitle2' sx={{ marginBottom: 2 }}>
               Потвърди нова парола
             </Typography>
             <TextField
-              label="Нова парола *"
-              variant="outlined"
+              label='Нова парола *'
+              variant='outlined'
               name={'rePassword'}
-              type={'password'}
+              type='password'
               fullWidth
               value={formik.values.rePassword}
-              onChange={formik.handleChange}
+              onChange={handleChange}
               error={formik.touched.rePassword && Boolean(formik.errors.rePassword)}
               helperText={formik.touched.rePassword && formik.errors.rePassword}
             />
           </Grid>
 
           <Grid item container xs={12}>
-            <Box
-              display="flex"
-              flexDirection={{ xs: 'column', sm: 'row' }}
-              alignItems={{ xs: 'stretched', sm: 'center' }}
-              justifyContent={'space-between'}
-              width={1}
-              margin={'0 auto'}
-            >
+            <Box className={styles.buttonBoxContainer} >
               <Box marginBottom={{ xs: 1, sm: 0 }}>
-
+                {successState === SUCCESS_STATES.success &&
+                  <Alert
+                    className={styles.fullWidth}
+                    severity="success">
+                    Промените са запазени в системата.
+                  </Alert>
+                }
+                {formik.status &&
+                  <Alert
+                    className={styles.fullWidth}
+                    severity="error">
+                    {formik.status}
+                  </Alert>
+                }
               </Box>
               <Button
-                size={'large'}
-                variant={'contained'}
-                type={'submit'}
-                startIcon={<CloudUploadOutlinedIcon />}
+                size='large'
+                variant='contained'
+                type='submit'
+                disabled={isLoading}
+                startIcon={isLoading
+                  ?
+                  <CircularProgress size={22} />
+                  :
+                  <CloudUploadOutlinedIcon />}
               >
                 Запази промените
               </Button>
