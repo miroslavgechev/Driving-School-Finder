9cacb0bb8a0e71b8af287c8cfa8cd663140e7738
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-01 12:49:55 +0200

    Create Edit School component

---
+++ driving-school-finder/src/components/EditSchool/EditSchool.js
@@ -0,0 +1,158 @@
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+// import Skeleton from '@mui/material/Skeleton';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import Container from 'components/Container';
+import Headline from '../shared/Headline/Headline';
+import SchoolForm from './components/SchoolForm/SchoolForm';
+import ContactsForm from './components/ContactsForm/ContactsForm';
+import ImgForm from './components/ImgForm/ImgForm';
+import CoursesForm from './components/CoursesForm/CoursesForm';
+import ConfirmForm from './components/ConfirmForm/ConfirmForm';
+
+import styles from './editSchool.module.css';
+
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useAuthContext } from 'contexts/authContext';
+
+import { getSchoolByOwnerUidAndSchoolId } from 'services/firestoreService';
+
+const EditSchool = () => {
+
+  const [school, setSchool] = useState(null);
+  const { user } = useAuthContext();
+  const schoolUid = useParams().id;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchSchool = async () => {
+      try {
+        const school = await getSchoolByOwnerUidAndSchoolId(user.uid, schoolUid);
+        setSchool(school);
+
+        if (!school) {
+          throw new Error('School not found or you are not the owner');
+        }
+
+      } catch (error) {
+        navigate('/notfound', { replace: true });
+      }
+    };
+    if (user) fetchSchool();
+
+  }, [user]);
+
+  return (
+
+    <>
+      {!school &&
+        <Box className={styles.fullPage}>
+          <CircularProgress />
+        </Box>
+      }
+
+      {school &&
+        <>
+          <Box bgcolor='alternate.main'>
+            <Container paddingY={{ xs: 2, sm: 2.5 }}>
+              <Headline text={'Редакция на автошкола'} />
+            </Container>
+          </Box>
+          <Container>
+
+            <Box>
+              <Grid container spacing={4}>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="h6" gutterBottom className={styles.headerText}>
+                    Данни за автошколата
+                  </Typography>
+                  <Typography variant='subtitle2' color='text.secondary'>
+                    Опиши своята автошкола, така че курсистите да разберат защо е най-подходяща за тях
+                  </Typography>
+                </Grid>
+
+                <Grid item xs={12} md={8}>
+                  <SchoolForm />
+                </Grid>
+
+              </Grid>
+
+              <Divider sx={{ marginY: 4 }} />
+
+              <Grid container spacing={4}>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="h6" gutterBottom className={styles.headerText}>
+                    Данни за контакт
+                  </Typography>
+                  <Typography variant='subtitle2' color='text.secondary'>
+                    Въведи контактните данни на твоята автошкола
+                  </Typography>
+                </Grid>
+
+                <Grid item xs={12} md={8}>
+                  <ContactsForm />
+                </Grid>
+              </Grid>
+
+              <Divider sx={{ marginY: 4 }} />
+              <Grid container spacing={4}>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="h6" gutterBottom className={styles.headerText}>
+                    Снимки
+                  </Typography>
+                  <Typography variant={'subtitle2'} color={'text.secondary'}>
+                    Качи снимки с високо качество, показвайки възможностите на школата
+                  </Typography>
+                </Grid>
+
+                <Grid item xs={12} md={8}>
+                  <ImgForm />
+                </Grid>
+
+              </Grid>
+              <Divider sx={{ marginY: 4 }} />
+
+              <Grid container spacing={4}>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="h6" gutterBottom className={styles.headerText}>
+                    Курсове
+                  </Typography>
+                  <Typography variant={'subtitle2'} color={'text.secondary'}>
+                    Опиши курсовете с тяхната продължителност и цени
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} md={12}>
+                  {
+                    school?.categoriesServed?.length > 0
+                      ?
+                      <CoursesForm key={school?.categoriesServed?.length} />
+                      :
+                      <Typography variant="h6" gutterBottom className={`${styles.headerText} ${styles.centerText}`}>
+                        Все още не си избрал категории, които предлага автошколата
+                      </Typography>
+                  }
+                </Grid>
+              </Grid>
+
+              <Divider sx={{ marginY: 4 }} />
+              <Grid container spacing={4}>
+                <Grid item xs={12} md={12}>
+                  <ConfirmForm />
+                </Grid>
+              </Grid>
+
+
+            </Box>
+          </Container>
+        </>
+      }
+    </>
+
+  );
+};
+
+export default EditSchool;
---
+++ driving-school-finder/src/components/EditSchool/components/ImgForm/ImgForm.js
@@ -0,0 +1,228 @@
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
+import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
+import CircularProgress from '@mui/material/CircularProgress';
+import Alert from '@mui/material/Alert';
+import FormHelperText from '@mui/material/FormHelperText';
+
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import styles from './imgForm.module.css';
+
+import { SUCCESS_STATES } from 'CONSTANTS';
+import fileMapper from 'utils/fileMapper';
+import { useSetSchoolContext } from 'contexts/setSchoolContext';
+
+const validationSchema = yup.object({
+  mainImage: yup
+    .mixed()
+    .required('Добавяне на основна снимка е задължително')
+    .test('type', 'Изображението трябва да бъде снимка', value => {
+      return value && ['image/jpg', 'image/jpeg', 'image/png'].includes(value.type);
+    }),
+  supportImages: yup.array().of(
+    yup
+      .mixed()
+      .test('type', 'Изображението трябва да бъде снимка', value => {
+        return value && ['image/jpg', 'image/jpeg', 'image/png'].includes(value.type);
+      })
+    // .required('Добавяне на поне една допълнителна снимка е задължително')
+  )
+  // .min(1, 'Добавяне на поне една допълнителна снимка е задължително'),
+});
+
+export const ImgForm = () => {
+
+  const [images, setImages] = useState({ mainImage: null, images: [] });
+  const [isLoading, setIsLoading] = useState(false);
+  const [successState, setSuccessState] = useState(SUCCESS_STATES.none);
+  const { setSchoolImages, setSchoolFiles } = useSetSchoolContext();
+
+  const initialValues = {
+    mainImage: '',
+    supportImages: [],
+  };
+
+  const handleSubmit = () => {
+    try {
+      formik.setStatus(null);
+      setIsLoading(true);
+      setSchoolImages(images);
+      setTimeout(() => {
+        setIsLoading(false);
+        setSuccessState(SUCCESS_STATES.success);
+      }, 500);
+    } catch (error) {
+      setTimeout(() => {
+        setIsLoading(false);
+        setSuccessState(SUCCESS_STATES.error);
+      }, 1000);
+    }
+  };
+
+  const handleImageChange = async (event, name) => {
+    setSuccessState(SUCCESS_STATES.error);
+
+    if (name === 'mainImage') {
+      setSchoolFiles(name, event.target.files[0]);
+      formik.setFieldValue(name, event.target.files[0]);
+      const url = await fileMapper([event.target.files[0]]);
+      setImages(data => ({ ...data, mainImage: url[0] }));
+    } else {
+      const firstFourImages = Array.from(event.target.files).slice(0, 4);
+      setSchoolFiles(name, firstFourImages);
+      formik.setFieldValue(name, firstFourImages);
+      const urlsArray = await fileMapper(firstFourImages);
+      setImages(data => ({ ...data, supportImages: urlsArray }));
+    }
+  };
+
+  const handleImageDelete = (prop) => {
+    if (prop === 'mainImage') {
+      setImages(data => ({ ...data, mainImage: null }));
+      formik.setFieldValue('mainImage', '');
+    } else {
+      const newImages = Array.from(images.supportImages).filter((_, i) => i !== prop);
+      setImages(data => ({ ...data, supportImages: newImages }));
+      formik.setFieldValue('supportImages', newImages);
+    }
+
+    setSuccessState(SUCCESS_STATES.error);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleSubmit,
+  });
+
+  return (
+    <Box>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={12}>
+            <Typography
+              variant='subtitle2'
+              className={styles.headerText}
+            >
+              Качи снимки
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Button
+              variant='contained'
+              component='label'
+              size='large'
+              startIcon={<AddPhotoAlternateOutlinedIcon />}
+              fullWidth
+            >
+              Избери основна снимка
+              <input
+                type='file'
+                hidden
+                name='mainImage'
+                multiple
+                accept='image/*'
+                onChange={(event) => handleImageChange(event, 'mainImage')}
+              />
+
+            </Button>
+            {formik.errors.mainImage
+              ?
+              <FormHelperText error>
+                {formik.touched.mainImage && formik.errors.mainImage}
+              </FormHelperText>
+              :
+              null}
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Button
+              variant='contained'
+              component='label'
+              size='large'
+              startIcon={<AddPhotoAlternateOutlinedIcon />}
+              fullWidth
+            >
+              Избери до 4 допълнителни снимки
+              <input
+                type='file'
+                hidden
+                name='supportImages'
+                multiple
+                accept='image/*'
+                onChange={(event) => handleImageChange(event, 'supportImages')}
+              />
+            </Button>
+            {formik.errors.supportImages
+              ?
+              <FormHelperText error>
+                {formik.touched.supportImages && formik.errors.supportImages}
+              </FormHelperText>
+              :
+              null}
+          </Grid>
+
+          <Grid item container xs={12} sm={12} className={styles.imageContainer}>
+            {images?.mainImage &&
+              <Box className={styles.imageListContainer}>
+                <img src={images.mainImage} alt="Main" className={styles.image} />
+                <Button onClick={() => handleImageDelete('mainImage')}>Изтрий</Button>
+              </Box>
+            }
+            {images?.supportImages?.length > 0 && Array.from(images.supportImages).map((url, index) => (
+              <Box
+                key={index}
+                className={styles.imageListContainer}
+              >
+                <img src={url} alt={`Additional ${index}`} className={styles.image} />
+                <Button onClick={() => handleImageDelete(index)}>Изтрий</Button>
+              </Box>
+            ))}
+          </Grid>
+
+          <Grid item container xs={12}>
+            <Box className={styles.buttonBoxContainer}>
+              <Box marginBottom={{ xs: 1, sm: 0 }}>
+                {successState === SUCCESS_STATES.success &&
+                  <Alert
+                    className={styles.fullWidth}
+                    severity="success">
+                    Промените са запазени локално
+                  </Alert>
+                }
+                {successState === SUCCESS_STATES.error &&
+                  <Alert
+                    className={styles.fullWidth}
+                    severity="error">
+                    Промените не са запазени
+                  </Alert>
+                }
+              </Box>
+              <Button
+                size='large'
+                variant='contained'
+                type='submit'
+                disabled={isLoading}
+                startIcon={isLoading
+                  ?
+                  <CircularProgress size={22} />
+                  :
+                  <CloudUploadOutlinedIcon />}
+              >
+                Запази промените
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </form>
+    </Box >
+  );
+};
+
+export default ImgForm;
