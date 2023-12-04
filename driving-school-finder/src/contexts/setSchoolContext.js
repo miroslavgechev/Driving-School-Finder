18fbde36cd08f491c5f032a5b13da68b9152e20a
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-04 16:50:15 +0200

    Updated upload school to create directory for reviews

--- driving-school-finder/src/contexts/setSchoolContext.js
+++ driving-school-finder/src/contexts/setSchoolContext.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useContext } from 'react';
 import { uploadFile } from 'services/firebaseStorageService';
-import { addSchool } from 'services/firestoreService';
+import { addEmptyReviewDirectory, addSchool } from 'services/firestoreService';
 
 const SetSchoolContext = createContext();
 
@@ -71,6 +71,7 @@ export const SetSchoolProvider = ({ children }) => {
       await uploadImage('mainImage', files?.mainImage);
       await uploadImage('supportImages', files?.supportImages);
       await addSchool(school);
+      await addEmptyReviewDirectory(school.ownerUid);
       setFiles(null);
     } catch (error) {
       throw new Error(error);
