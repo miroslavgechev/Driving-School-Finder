5db8c409a9c0012733a072a4db06a0b867f38f83
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-04 16:49:32 +0200

    Added logic to reviews component

--- driving-school-finder/src/components/SchoolDetails/components/Reviews/Reviews.js
+++ driving-school-finder/src/components/SchoolDetails/components/Reviews/Reviews.js
@@ -9,49 +9,51 @@ import Rating from '@mui/material/Rating';
 import FeedbackForm from './components/FeedbackForm/FeedbackForm';
 import FeedbackList from './components/FeedbackList/FeedbackList';
 
-const Reviews = ({ ratingScore, ratingCount, reviews }) => {
+import styles from './reviews.module.css';
+
+const Reviews = ({ ratingScore, ratingCount, reviews, schoolUid }) => {
 
   const [openToReview, setOpenToReview] = useState(false);
   const [openFeedbackList, setOpenFeedbackList] = useState(false);
 
   return (
-    <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
+    <Box className={styles.mainContainer}>
       <Grid container spacing={4}>
         <Grid item xs={12}>
-          <Typography variant={'h5'} fontWeight={700}>
+          <Typography variant='h5' className={styles.headerText}>
             Отзиви от курсисти
           </Typography>
-          <Box display={'flex'} alignItems={'center'} marginY={2}>
-            <Typography variant={'h2'} fontWeight={700} marginRight={1}>
+          <Box className={styles.ratingBox} marginY={2}>
+            <Typography variant='h2' className={styles.headerText} marginRight={1}>
               {ratingScore}
             </Typography>
             <Box>
-              <Box display={'flex'} alignItems={'center'}>
+              <Box className={styles.ratingBox}>
                 <Rating
-                  name="text-feedback"
+                  name='text-feedback'
                   value={ratingScore}
                   readOnly
                   precision={0.5}
-                  fontSize="inherit"
+                  fontSize='inherit'
                   size="large"
                 />
               </Box>
-              <Typography color={'text.secondary'}>
+              <Typography color='text.secondary'>
                 Общо {ratingCount} оценки
               </Typography>
             </Box>
           </Box>
-          <Stack direction={'row'} spacing={2}>
+          <Stack direction='row' spacing={2}>
             <Button
-              size={'large'}
-              variant={'outlined'}
+              size='large'
+              variant='outlined'
               onClick={() => setOpenFeedbackList(true)}
             >
               Виж всички
             </Button>
             <Button
-              size={'large'}
-              variant={'contained'}
+              size='large'
+              variant='contained'
               sx={{
                 marginTop: { xs: 2, md: 0 },
               }}
@@ -63,24 +65,24 @@ const Reviews = ({ ratingScore, ratingCount, reviews }) => {
         </Grid>
         {reviews.map((item, i) => (
           <Grid key={i} xs={12} sm={6} item>
-            <Box display={'flex'} alignItems={'center'}>
+            <Box className={styles.ratingBox}>
               <Rating
-                name="text-feedback"
+                name='text-feedback'
                 value={item.reviewScore}
                 readOnly
                 precision={0.5}
-                fontSize="inherit"
-                size="large"
+                fontSize='inherit'
+                size='large'
               />
             </Box>
-            <Typography variant={'caption'} color={'text.secondary'}>
+            <Typography variant='caption' color='text.secondary'>
               {`от ${item.fullName}, ${item.date}`}
             </Typography>
             <Typography marginY={1}>{item.review}</Typography>
           </Grid>
         ))}
       </Grid>
-      <FeedbackForm open={openToReview} onClose={() => setOpenToReview(false)} />
+      <FeedbackForm schoolUid={schoolUid} open={openToReview} onClose={() => setOpenToReview(false)} />
       <FeedbackList open={openFeedbackList} onClose={() => setOpenFeedbackList(false)} reviews={reviews} />
 
     </Box>
