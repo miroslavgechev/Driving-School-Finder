49cd9e4725e7c03eb6c843a1b95604825e9ce79d
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-05 19:40:02 +0200

    Added logic to EditFeedbackForm

--- driving-school-finder/src/components/ManageAccount/components/ManageReviews/EditFeedbackForm/EditFeedBackForm.js
+++ driving-school-finder/src/components/ManageAccount/components/ManageReviews/EditFeedbackForm/EditFeedBackForm.js
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Dialog from '@mui/material/Dialog';
@@ -8,19 +6,86 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
+import CircularProgress from '@mui/material/CircularProgress';
+import Alert from '@mui/material/Alert';
 
-const mockSchoolName = {
-  name: 'Училище 1',
-};
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
-const EditFeedbackForm = ({ onClose, open }) => {
-  const [currentScore, setCurrentScore] = useState(3);
+import styles from './editFeedbackForm.module.css';
+import { SUCCESS_STATES } from 'CONSTANTS';
+import { updateReviewByReviewId } from 'services/firestoreService';
+
+const validationSchema = yup.object({
+  rating: yup
+    .number()
+    .transform(value => (isNaN(value) ? undefined : value))
+    .required('Оценката е задължителна')
+    .positive('Оценката трябва да е положително число')
+    .integer('Оценката трябва да е цяло число'),
+  feedback: yup
+    .string()
+    .trim()
+    .required('Въвеждането на отзив е задължително')
+    .min(1, 'Отзива следва да е поне един символ')
+    .max(500, 'Отзива не може да е повече от 500 символа'),
+});
+
+const EditFeedbackForm = ({ onClose, open, reviewToEdit, setReviewToEdit }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [successState, setSuccessState] = useState(SUCCESS_STATES.none);
+
+  const initialValues = {
+    rating: Number(reviewToEdit.rating),
+    feedback: reviewToEdit.feedback,
+  };
+
+  const handleSubmit = async (values) => {
+    try {
+      formik.setStatus(null);
+      setIsLoading(true);
+
+      const review = {
+        fullName: reviewToEdit.fullName,
+        schoolId: reviewToEdit.schoolId,
+        userId: reviewToEdit.userId,
+        date: new Date().toISOString(),
+        ...values,
+      };
+
+      await updateReviewByReviewId(reviewToEdit.id, review);
+
+      setSuccessState(SUCCESS_STATES.success);
+
+      setTimeout(() => {
+        setIsLoading(false);
+        onClose();
+        setReviewToEdit(null);
+      }, 1000);
+
+    } catch (error) {
+      setIsLoading(false);
+      setSuccessState(SUCCESS_STATES.error);
+    }
+  };
+
+  const handleChange = (event) => {
+    formik.handleChange(event);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleSubmit,
+  });
 
   return (
     <Dialog
       onClose={onClose}
       open={open}
-      maxWidth={'sm'}
+      maxWidth='sm'
       sx={{
         '& .MuiPaper-root': {
           borderRadius: 2,
@@ -28,58 +93,95 @@ const EditFeedbackForm = ({ onClose, open }) => {
       }}
     >
       <Box paddingY={2} paddingX={4}>
-        <Box paddingY={2} display={'flex'} justifyContent={'space-between'}>
-          <Typography variant={'h5'} fontWeight={700}>
-            Редактирай оценка
+        <Box paddingY={2} className={styles.containerBox}>
+          <Typography variant='h5' className={styles.headerText}>
+            Редактирай отзив
           </Typography>
           <CloseOutlinedIcon
             onClick={onClose}
-            sx={{ cursor: 'pointer' }}
+            className={styles.cursorPointer}
           />
         </Box>
 
         <Box paddingY={2}>
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
-                <Typography variant={'subtitle1'} fontWeight={700} sx={{ marginBottom: 1 }}>
-                  Даваш оценка за {mockSchoolName.name}
+                <Typography variant='subtitle1' className={styles.headerText} sx={{ marginBottom: 1 }}>
+                  Редактираш отзив за {reviewToEdit.schoolName}
                 </Typography>
               </Grid>
 
               <Grid item xs={12}>
-                <Typography variant={'subtitle2'} sx={{ marginBottom: 1 }}>
+                <Typography variant='subtitle2' sx={{ marginBottom: 1 }}>
                   Оценка
                 </Typography>
 
-                <Box display={'flex'} alignItems={'center'}>
+                <Box className={styles.containerBox}>
                   <Rating
-                    name="rating-feedback"
-                    value={currentScore}
+                    name="rating"
                     precision={1}
                     fontSize="inherit"
                     size="large"
-                    onChange={(e, newValue) => setCurrentScore(newValue)}
+                    value={Number(formik.values.rating)}
+                    onChange={handleChange}
                   />
+                  {formik.touched.rating && formik.errors.rating &&
+                    <FormHelperText error>{formik.errors.rating}</FormHelperText>
+                  }
                 </Box>
               </Grid>
               <Grid item xs={12}>
-                <Typography variant={'subtitle2'} sx={{ marginBottom: 1 }}>
+                <Typography variant='subtitle2' sx={{ marginBottom: 1 }}>
 
                 </Typography>
                 <TextField
-                  label="Отзив *"
-                  variant="outlined"
-                  name={'feedback'}
-                  fullWidth
+                  label='Отзив *'
+                  variant='outlined'
+                  name='feedback'
+                  className={styles.fullWidth}
                   multiline
                   rows={5}
+                  value={formik.values.feedback}
+                  onChange={handleChange}
+                  error={formik.touched.feedback && Boolean(formik.errors.feedback)}
+                  helperText={formik.touched.feedback && formik.errors.feedback}
                 />
               </Grid>
               <Grid item container xs={12}>
-                <Button size={'large'} variant={'contained'} type={'submit'}>
-                  Запази
-                </Button>
+                <Box className={styles.buttonBoxContainer}>
+                  <Button
+                    size='large'
+                    variant='contained'
+                    type='submit'
+                    disabled={isLoading}
+                    startIcon={isLoading
+                      ?
+                      <CircularProgress size={22} />
+                      :
+                      <CloudUploadOutlinedIcon />}
+                  >
+                    Запази промените
+                  </Button>
+
+                  <Box marginBottom={{ xs: 1, sm: 0 }}>
+                    {successState === SUCCESS_STATES.success &&
+                      <Alert
+                        className={styles.fullWidth}
+                        severity="success">
+                        Твоят отзив е получен!
+                      </Alert>
+                    }
+
+                    {successState === SUCCESS_STATES.error &&
+                      <Alert
+                        className={styles.fullWidth}
+                        severity="error">
+                        Нещо се обърка, твоят отзив не е получен...
+                      </Alert>
+                    }
+                  </Box>
+                </Box>
               </Grid>
             </Grid>
           </form>
