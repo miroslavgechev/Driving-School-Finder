52f40108e7266ec469f40fa92ef7d307b0a1bbe2
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-07 16:18:14 +0200

    Add css styling to component

--- driving-school-finder/src/components/Catalog/Catalog.js
+++ driving-school-finder/src/components/Catalog/Catalog.js
@@ -1,26 +1,42 @@
-// import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 
 import Container from 'components/Container';
+import { alpha, useTheme } from '@mui/material/styles';
 
 import FeaturedSchools from './components/FeaturedSchools/Schools';
 import FilterBar from './components/FilterBar/FilterBar';
-import Hero from './components/Hero/Hero';
+import Header from './components/Header/Header';
 
-// import { getSchools, getSpecificFieldOfAllSchools, getSchoolByName } from 'services/firebaseFirestoreTest';/
-// import { addSchool } from 'services/firebaseFirestoreTest';
+import styles from './catalog.module.css';
 
 const Catalogue = () => {
-
+  const theme = useTheme();
 
   return (
     <>
+      <Box
+        className={styles.mainContainer}
+        sx={{
+          backgroundColor: theme.palette.alternate.main,
+        }}
+      >
+        <Box
+          className={styles.backgroundImageFilter}
+          sx={{
+            background: alpha(theme.palette.primary.dark, 0.3),
+          }}
+        />
+      </Box>
 
-      <Hero />
+      <Header />
+
       <FilterBar />
 
-
-      <Box bgcolor={'alternate.main'}>
+      <Box
+        sx={{
+          backgroundColor: theme.palette.alternate.main,
+        }}
+      >
         <Container>
           <FeaturedSchools />
         </Container>
