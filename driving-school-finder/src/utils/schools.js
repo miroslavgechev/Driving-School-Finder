bec468f5af29d36541b23d6153b98c3bca765257
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-08 19:51:04 +0200

    Add function to filterSchools

--- driving-school-finder/src/utils/schools.js
+++ driving-school-finder/src/utils/schools.js
@@ -25,6 +25,48 @@ export const matchSchoolsWithRatings = (schools, ratings) => {
   }
 };
 
+export const filterSchools = (schools, filter) => {
+
+  if (!schools || !filter) {
+    return;
+  }
+
+  const { name, category, rating, region } = filter;
+
+  const filteredSchools = schools?.filter((school) => {
+    let isNameMatch = true;
+    let isCategoryMatch = true;
+    let isRatingMatch = true;
+    let isRegionMatch = true;
+
+    if (name) {
+      isNameMatch = school.name.toLowerCase().includes(name.toLowerCase());
+    }
+
+    if (!category || category === 'Всички') {
+      isCategoryMatch = true;
+    } else {
+      isCategoryMatch = school.categoriesServed.includes(category);
+    }
+
+    if (!rating || rating.label === 'Всички') {
+      isRatingMatch = true;
+    } else {
+      isRatingMatch = school.ratingScore >= rating.rating;
+    }
+
+    if (!region || region === 'Всички') {
+      isRegionMatch = true;
+    } else {
+      isRegionMatch = school.regionsServed.includes[region] || school.regionsServed.includes('Всички');
+    }
+
+    return isNameMatch && isCategoryMatch && isRatingMatch && isRegionMatch;
+  });
+
+  return filteredSchools;
+};
+
 export const sortDesc = (schools, sortBy) => {
   try {
     schools.sort((a, b) => b[sortBy] - a[sortBy]);
