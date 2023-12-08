a9d4288a1230455fbb00671dda5b3229f8b9a34d
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-08 16:06:19 +0200

    Added logic to CatalogList component

--- driving-school-finder/src/components/Catalog/components/SchoolsList/SchoolsList.js
+++ driving-school-finder/src/components/Catalog/components/SchoolsList/SchoolsList.js
@@ -11,99 +11,65 @@ import Rating from '@mui/material/Rating';
 import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
 import DirectionsCarFilledOutlinedIcon from '@mui/icons-material/DirectionsCarFilledOutlined';
 import EastRoundedIcon from '@mui/icons-material/EastRounded';
+import CircularProgress from '@mui/material/CircularProgress';
 // import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+
+
+import { Link } from 'react-router-dom';
 
 import { useTheme } from '@mui/material/styles';
 import styles from './schoolsList.module.css';
 
-const mock = [
-  {
-    media: 'https://images.pexels.com/photos/7433/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Автошкола Иванов',
-    categories: ['AM', 'A', 'A1', 'A2', 'B1', 'B', 'BA'],
-    location: 'Всички',
-    value: 2
-  },
-  {
-    media: 'https://images.pexels.com/photos/804130/pexels-photo-804130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Автошкола Петров',
-    categories: ['AM', 'A', 'A1', 'A2', 'B1', 'B', 'BA'],
-    location: 'Слатина, Средец',
-    value: 4
-  },
-  {
-    media: 'https://images.pexels.com/photos/543602/pexels-photo-543602.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-    title: 'Автошкола Георгиев',
-    categories: ['AM', 'A', 'A1', 'A2'],
-    location: 'Всички',
-    value: 5
-  },
-  {
-    media: 'https://images.pexels.com/photos/1051071/pexels-photo-1051071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Автошкола Димитров',
-    categories: ['B1', 'B', 'BA'],
-    location: 'Всички',
-    value: 4
-  },
-  {
-    media: 'https://images.pexels.com/photos/1051071/pexels-photo-1051071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Loft design',
-    categories: ['AM', 'A', 'A1', 'A2', 'B1', 'B', 'BA'],
-    location: 'Всички',
-    value: 5
-  },
-  {
-    media: 'https://images.pexels.com/photos/13781/pexels-photo-13781.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'Автошкола Гечев',
-    categories: ['AM', 'A', 'A1', 'A2', 'B1', 'B', 'BA'],
-    location: 'Всички',
-    value: 4.5
-  },
-];
-
-const SchoolsList = () => {
+const SchoolsList = ({ schoolsForCatalog }) => {
   const theme = useTheme();
+  console.log(schoolsForCatalog);
 
   return (
     <Box paddingTop={{ xs: 0, md: 4 }}>
-
-      <Grid container spacing={4}>
-        {mock.map((item, i) => (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            key={i}
-            data-aos={'fade-up'}
-            data-aos-delay={i * 100}
-            data-aos-offset={100}
-            data-aos-duration={600}
-          >
-            <Box
-              component={Card}
-              className={styles.cardContainer}
+      {!schoolsForCatalog &&
+        <Box className={styles.spinner}>
+          <CircularProgress />
+        </Box>
+      }
+      {schoolsForCatalog &&
+        <Grid container spacing={4}>
+          {schoolsForCatalog?.map((school, i) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              key={i}
+              data-aos={'fade-up'}
+              data-aos-delay={i * 100}
+              data-aos-offset={100}
+              data-aos-duration={600}
             >
-              <CardMedia
-                title={item.title}
-                image={item.media}
-                className={styles.cardMedia}
+              <Box
+                component={Card}
+                className={styles.cardContainer}
               >
-                <Box
-                  className={styles.overMediaItemsContainer}
-                  padding={2}
+                <CardMedia
+                  title={school.name}
+                  image={school.mainImage}
+                  className={styles.cardMedia}
                 >
                   <Box
-                    className={styles.ratingContainer}
-                    padding={1}
-                    bgcolor={'background.paper'}
-                    boxShadow={1}
-                    borderRadius={2}
+                    className={styles.overMediaItemsContainer}
+                    padding={2}
                   >
-                    <Rating name='read-only' value={item.value} readOnly />
-                  </Box>
+                    <Box
+                      className={styles.ratingContainer}
+                      padding={1}
+                      bgcolor={'background.paper'}
+                      boxShadow={1}
+                      borderRadius={2}
+                    >
+                      <Rating name='read-only' value={Number(school.ratingScore)} readOnly />
+                    </Box>
 
-                  {/* <Box
+                    {/* To use for add to favourites future functionality: */}
+                    {/* <Box
                     className={styles.favouritesContainer}
                     padding={1}
                     bgcolor={'background.paper'}
@@ -115,61 +81,68 @@ const SchoolsList = () => {
                     />
                   </Box> */}
 
-                </Box>
-              </CardMedia>
+                  </Box>
+                </CardMedia>
 
-              <CardContent>
-                <Typography
-                  className={styles.schoolTitle}
-                  variant='h6'
-                >
-                  {item.title}
-                </Typography>
-
-                <Box
-                  className={styles.schoolItemsContainer}
-                  marginY={2}
-                >
-                  <PlaceOutlinedIcon
-                    className={styles.itemIcon}
-                    sx={{
-                      color: theme.palette.text.secondary,
-                    }}
-                  />
-                  <Typography variant='subtitle2' color='text.secondary'>
-                    {item.location}
-                  </Typography>
-                </Box>
-
-                <Box className={styles.schoolItemsContainer}>
-                  <DirectionsCarFilledOutlinedIcon
-                    className={styles.itemIcon}
-                    sx={{
-                      color: theme.palette.text.secondary,
-                    }}
-                  />
-                  <Typography variant='subtitle2' color='text.secondary'>
-                    {item.categories.join(', ')}
-                  </Typography>
-                </Box>
-
-                <CardActions className={styles.cardActions}>
-                  <Button
-                    endIcon={
-                      <EastRoundedIcon
-                        className={styles.itemIcon}
-                      />
-                    }
+                <CardContent>
+                  <Typography
+                    className={styles.schoolTitle}
+                    variant='h6'
                   >
-                    Виж повече
-                  </Button>
-                </CardActions>
-              </CardContent>
-            </Box>
-          </Grid>
-        ))}
+                    {school.name}
+                  </Typography>
 
-        <Grid item xs={12}>
+                  <Box
+                    className={styles.schoolItemsContainer}
+                    marginY={2}
+                  >
+                    <PlaceOutlinedIcon
+                      className={styles.itemIcon}
+                      sx={{
+                        color: theme.palette.text.secondary,
+                      }}
+                    />
+                    <Typography variant='subtitle2' color='text.secondary'>
+                      {
+                        school.regionsServed.includes('Всички')
+                          ?
+                          'Всички'
+                          :
+                          school.regionsServed.join(', ').slice(0, 40) + '...'
+                      }
+                    </Typography>
+                  </Box>
+
+                  <Box className={styles.schoolItemsContainer}>
+                    <DirectionsCarFilledOutlinedIcon
+                      className={styles.itemIcon}
+                      sx={{
+                        color: theme.palette.text.secondary,
+                      }}
+                    />
+                    <Typography variant='subtitle2' color='text.secondary'>
+                      {school.categoriesServed.join(', ')}
+                    </Typography>
+                  </Box>
+
+                  <CardActions className={styles.cardActions}>
+                    <Button
+                      component={Link}
+                      to={`/school/${school.ownerUid}`}
+                      endIcon={
+                        <EastRoundedIcon className={styles.itemIcon} />
+                      }
+                    >
+                      Виж повече
+                    </Button>
+                  </CardActions>
+                </CardContent>
+              </Box>
+            </Grid>
+          ))}
+
+          {/* Use in case of more schools than the current limit: */}
+          {/* <Grid item xs={12}>
           <Box
             className={styles.loadMoreBtnContainer}
             marginTop={2}
@@ -183,9 +156,11 @@ const SchoolsList = () => {
               Зареди още...
             </Button>
           </Box>
+        </Grid> */}
         </Grid>
-      </Grid>
+      }
     </Box >
+
   );
 };
 
