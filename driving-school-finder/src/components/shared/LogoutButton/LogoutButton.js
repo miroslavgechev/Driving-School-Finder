8bfc7532339483c2bf84870d5609a1fc440001ce
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-08 09:21:02 +0200

    Add redirection on user logout plus reseting own school data

--- driving-school-finder/src/components/shared/LogoutButton/LogoutButton.js
+++ driving-school-finder/src/components/shared/LogoutButton/LogoutButton.js
@@ -1,20 +1,26 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import CircularProgress from '@mui/material/CircularProgress';
 import Button from '@mui/material/Button';
 import { useAuthContext } from 'contexts/authContext';
+import { useSetSchoolContext } from 'contexts/setSchoolContext';
 import styles from './logoutButton.module.css';
 
 const LogoutButton = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const { logout } = useAuthContext();
+  const { resetSchool } = useSetSchoolContext();
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
       setIsLoading(true);
       await logout();
+      resetSchool();
       setIsLoading(false);
+      navigate('/');
     } catch (err) {
       console.log(err.message);
     }
