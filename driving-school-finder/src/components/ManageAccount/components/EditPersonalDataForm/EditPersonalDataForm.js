69502b63467d80abd48c454b58d1c79cf79def56
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-11 23:11:52 +0200

    Fix a bug where user names change wrongly back to a previous version
    This reverts commit 9d91e0bb9ccb0774022f70ef401ddea533f3c5ac.

--- driving-school-finder/src/components/ManageAccount/components/EditPersonalDataForm/EditPersonalDataForm.js
+++ driving-school-finder/src/components/ManageAccount/components/EditPersonalDataForm/EditPersonalDataForm.js
@@ -14,6 +14,7 @@ import SubmitButton from 'components/shared/SubmitButton/SubmitButton';
 
 import { useAuthContext } from 'contexts/authContext';
 import { SUCCESS_STATES, ALERT_SEVERITY } from 'CONSTANTS';
+import { updateCustomUserData } from 'services/firestoreService';
 import styles from './editPersonalDataForm.module.css';
 
 const validationSchema = yup.object({
@@ -37,7 +38,7 @@ const validationSchema = yup.object({
 });
 
 const EditPersonalDataForm = () => {
-  const { user, updateUserData } = useAuthContext();
+  const { user } = useAuthContext();
 
   const initialValues = {
     firstName: user?.firstName,
@@ -54,7 +55,7 @@ const EditPersonalDataForm = () => {
       formik.setStatus(null);
       setIsLoading(true);
 
-      await updateUserData(user.uid, values);
+      await updateCustomUserData(user.uid, values);
 
       setIsLoading(false);
       setSuccessState(SUCCESS_STATES.success);
