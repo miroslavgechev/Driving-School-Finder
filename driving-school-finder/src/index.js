3988bb04b34c9f37cd62cecc1005e9d574cd2001
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-17 12:40:40 +0200

    Reenable strict mode for dev purposes

--- driving-school-finder/src/index.js
+++ driving-school-finder/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import reportWebVitals from './reportWebVitals';
 import App from './App';
@@ -6,9 +6,9 @@ import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
